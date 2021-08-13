18ca70c13c1dc5fdc1059b7e54d85ecc0f3736e9
LifeStyleRolePlay <88177315+LifeStyleRolePlay@users.noreply.github.com>
2021-08-13 00:55:22 -0500

    Add files via upload

---
+++ qb-streetfight/config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.BLUEZONE = {x = -520.63, y = -1712.40, z = 19.45}
+Config.REDZONE = {x = -514.79, y = -1711.49, z = 19.45}
+Config.CENTER = {x = -517.61, y = -1712.04, z = 20.45}
+
+Config.DISTANCE = 10 -- Distance to see fight indicators
+Config.TP_DISTANCE = 6 -- Minimum distance between fight and spectators
+Config.LEAVE_FIGHT_DISTANCE = 15 -- Distance to quit fight
+Config.DISTANCE_INTERACTION = 2.0 -- Interaction for fighter events
+Config.E_KEY = 38 -- Action key 'E' default
+
+Config.BLIP = {
+    coords = {x = -517.61, y = -1712.04, z = 20.45},
+    text   = 'Boxing Ring',
+    sprite = 491,
+    color  = 1,
+    scale  = 0.8
+}
